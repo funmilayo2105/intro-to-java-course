88ff3558e35613c89565df3a68410e77f098a0aa
Funmilayo Kolawole <layokay@outlook.com>
2024-08-22 19:52:09 +0100

    adding FizzBuzz test

---
+++ exercises/test-driven-development/src/main/java/com/cbfacademy/FizzBuzz.java
@@ -0,0 +1,15 @@
+package com.cbfacademy;
+public class FizzBuzz {
+        public String get(int number) {
+            if (number % 15 == 0) {
+                return "FizzBuzz";
+            } else if (number % 3 == 0) {
+                return "Fizz";
+            } else if (number % 5 == 0) {
+                return "Buzz";
+            } else {
+                return String.valueOf(number);
+            }
+        }
+    
+}
--- exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
+++ exercises/test-driven-development/src/test/java/com/cbfacademy/AppTest.java
@@ -1,11 +1,11 @@
 package com.cbfacademy;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.notNullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 @DisplayName(value = "Basic Test Suite")
 public class AppTest {
@@ -17,4 +17,45 @@ public class AppTest {
 
         assertThat(app, is(notNullValue()));
     }
+    @Test
+
+void FizzBuzztest(){
+    FizzBuzz fizzbuzz = new FizzBuzz();
+    assertEquals("FizzBuzz", fizzbuzz.get(15));
+}
+@Test
+void Fizztest(){
+    FizzBuzz fizzbuzz = new FizzBuzz();
+    assertEquals("Fizz", fizzbuzz.get(3));
+    assertEquals("Fizz", fizzbuzz.get(6));
+    assertEquals("Fizz", fizzbuzz.get(9));
+    assertEquals("Fizz", fizzbuzz.get(12));
+
+
+
+}
+@Test
+void Buzztest(){
+    FizzBuzz fizzbuzz = new FizzBuzz();
+    assertEquals("Buzz", fizzbuzz.get(5));
+    assertEquals("Buzz", fizzbuzz.get(10));
+   
+}
+
+@Test
+void Numtest(){
+    FizzBuzz fizzbuzz = new FizzBuzz();
+    assertEquals("1", fizzbuzz.get(1));
+    assertEquals("2", fizzbuzz.get(2));
+    assertEquals("4", fizzbuzz.get(4));
+    assertEquals("7", fizzbuzz.get(7));
+    
+
+}
+
+@Test
+//years divisible by 400 are leap years
+void divisibleby400(){
+    
+}
 }
