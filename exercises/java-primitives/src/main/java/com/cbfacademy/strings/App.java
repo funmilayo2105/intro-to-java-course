e98e6031c514b348de397d9c3bfdf1386dc911a8
Funmilayo Kolawole <layokay@outlook.com>
2024-10-17 23:14:51 +0100

    doing assignment on app.java

--- exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
+++ exercises/java-primitives/src/main/java/com/cbfacademy/strings/App.java
@@ -1,22 +1,22 @@
 package com.cbfacademy.strings;
 
 public class App {
-    public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 
     public static String concatenate(String word1, String word2, String word3) {
-        // TODO: Write code that concatenates the input parameters and returns the result
-        throw new RuntimeException("Not implemented");
+        // Concatenate the three input strings and return the result
+        return word1 + word2 + word3;
     }
 
     public static Boolean areEqual(String word1, String word2) {
-        // TODO: Write code to determine whether the input parameters are equal strings
-        throw new RuntimeException("Not implemented");
+        // Determine whether the two input strings are equal
+        return word1.equals(word2);
     }
 
     public static String format(String item, int quantity, double price) {
-        // TODO: Write code to return a string formatted as follows: "Item: [item]. Price: £[price]. Quantity: [quantity]". The price should be formatted to two decimal places, e.g. 99.99
-        throw new RuntimeException("Not implemented");
+        // Format the string as "Item: [item]. Price: £[price]. Quantity: [quantity]"
+        return String.format("Item: %s. Price: £%.2f. Quantity: %d", item, price, quantity);
     }
 }
